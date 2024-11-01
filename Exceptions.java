bd3d436cedb733726157c50d77b1de560acf178d
David Hill <hilld3698@gmail.com>
2024-10-31 22:58:46 -0500

    Create Exceptions.java

---
+++ Exceptions.java
@@ -0,0 +1,11 @@
+class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
+
+class TaskNotFoundException extends Exception {
+    public TaskNotFoundException(String message) {
+        super(message);
+    }
+}
